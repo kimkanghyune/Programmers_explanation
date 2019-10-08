74e81f0c6dbd74487a0da5fd7ba900080a0f49cf
김강현 <nuclear_1@naver.com>
2019-10-08 17:16:40 +0900

    문제풀이 커밋

---
+++ Biggest_Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biggest_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution(new int[] { 3, 30, 34, 5, 9 });
+        }
+    }
+}
---
+++ Biggest_Number/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Biggest_Number
+{
+    public class Solution
+    {
+        public string solution(int[] numbers)
+        {
+            Array.Sort(numbers,
+                (a, b) => {
+                    return (b.ToString() + a.ToString()).CompareTo(a.ToString() + b.ToString());
+                });
+            if (numbers[0] == 0) return "0";
+            return string.Join("", numbers);
+        }
+    }
+}
