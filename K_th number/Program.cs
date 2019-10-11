ae4252ac97cb7d29889bb89deff58b1b41b553d1
김강현 <wwponv158@gmail.com>
2019-10-11 17:03:57 +0900

    풀이 추가, 문제설명 추가

---
+++ K_th number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_th_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            foreach (int i in solution.solution(
+                new int[] { 1, 5, 2, 6, 3, 7, 4 },
+                new int[,] { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } }
+                ))
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
