897c4ebb6a84316c335beafdc9fdf7478ddd40a6
김강현 <nuclear_1@naver.com>
2019-10-16 23:05:38 +0900

    H-index Commit

---
+++ H-index/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H_index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution(new int[] { 3, 0, 6, 1, 5 });
+            
+        }
+    }
+}
