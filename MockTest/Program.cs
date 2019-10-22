bf9f7715c1016f2eca05c28bb61b8d1cdfbb0850
김강현 <nuclear_1@naver.com>
2019-10-22 15:06:41 +0900

    MockTest 문제 풀이

---
+++ MockTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution(new int[] { 1, 2, 3, 4, 5 });
+        }
+    }
+}
