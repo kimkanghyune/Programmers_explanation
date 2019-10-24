c194860596aecaac6392fae636d89a350181f6ff
김강현 <nuclear_1@naver.com>
2019-10-24 11:07:51 +0900

    Find_Prime_Numbers Commit

---
+++ Find_Prime_Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Prime_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution("19989279");
+        }
+    }
+}
---
+++ Find_Prime_Numbers/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Prime_Numbers
+{
+    public class Solution
+    {
+        public int solution(string numbers)
+        {
+            int answer = 0;
+            List<int> Arr_numbers = new List<int>();
+
+            StringBuilder sb = new StringBuilder();
+            Arr_numbers = StringCombination(numbers, sb, 0);
+            Collections.
+
+            return answer;
+        }
+        List<int> StringCombination(string s, StringBuilder sb, int index)
+        {
+            List<int> Arr_numbers = new List<int>();
+            for (int i = index; i < s.Length; i++)
+            {
+                // 1) 한 문자 추가
+                sb.Append(s[i]);
+
+                // 2) 구한 문자조합 출력
+                Console.WriteLine(sb.ToString());
+
+                // 3) 나머지 문자들에 대한 조합 구하기
+                StringCombination(s, sb, i + 1);
+
+                // 위의 1에서 추가한 문자 삭제 
+                sb.Remove(sb.Length - 1, 1);
+            }
+            return Arr_numbers;
+        }
+    }
+}
