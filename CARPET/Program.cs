c527e5cb2d1b37859a60b3224d73e6dc904cec61
김강현 <wwponv158@gmail.com>
2020-07-14 18:16:48 +0900

    carpet commit

---
+++ CARPET/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARPET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solution(24,24);
+        }
+    }
+}
